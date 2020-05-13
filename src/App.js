f5e9c4e2c41f7c2133133f96d3c8dfad9f677ae5
shankar1525 <shankar@pronteff.com>
2020-05-13 18:33:42 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Unity, { UnityContent } from "react-unity-webgl";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // window.alert = function () {};
+    this.unityContent = new UnityContent(
+      "UnitySample/OutWebGL.json",
+      "UnitySample/UnityLoader.js"
+    );
+    this.state = {
+      value: 0,
+    };
+    this.unityContent.on("ShowMessage", (message) => {
+      // Now we can use the score to for example
+      // display it on our React app.
+
+      this.setState({ value: message });
+    });
+  }
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.unityContent.send("Main Camera", "ctrl", "1");
+    // }, 20000);
+  }
+  onClick() {
+    // this function sends a message to a game object
+    // named "SpawnController" to the public method
+    // "SpawnEnemies" with a value of "10".
+
+    this.unityContent.send("Main Camera", "ctrl", "1");
+  }
+  render() {
+    return (
+      <div style={{ width: window.innerWidth, height: window.innerHeight }}>
+        <button onClick={this.onClick.bind(this)}>Hello</button>
+        {/* <h5>unity value: {this.state.value}</h5> */}
+        <Unity unityContent={this.unityContent} />
+      </div>
+    );
+  }
 }
 
 export default App;
